45905282dd700cdc5e688df8180e7cc8337de333
MrEdLaMel <eddy12charles@gmail.com>
2021-01-31 21:30:05 -0800

    Ready for Review and Grading

--- ctec-127-lab-6.php
+++ ctec-127-lab-6.php
@@ -54,7 +54,7 @@
         }
         return $convertedTemp;
 
-    } // end function
+    } // end functio
 
     // Logic to check for POST and grab data from $_POST
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
