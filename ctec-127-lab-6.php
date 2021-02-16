881f92350aa0bed6d1f47e7594bc122d96cb62cf
MrEdLaMel <eddy12charles@gmail.com>
2021-02-15 17:48:12 -0800

    now

--- ctec-127-lab-6.php
+++ ctec-127-lab-6.php
@@ -72,7 +72,7 @@
     }
 
     // echo "originalTemperature = " . $originalTemperature . "<br>";
-    // echo "originalUnit = " . $originalUnit . "<br>" ;
+    // eccho "originalUnit = " . $originalUnit . "<br>" ;
     // echo "conversionUnit = " . $conversionUnit . "<br>";
     // echo "convertedTemp = " . $convertedTemp . "<br>"
     ?>
